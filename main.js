d8a79cfe8dae70c918fa34cbc7b2b024a5eb6182
Wellington2708 <wellingtondevelopercontact@gmail.com>
2022-10-31 16:24:52 +0000

    atualizando revisão de array e repeticao com for e while

--- main.js
+++ main.js
@@ -53,3 +53,41 @@ var alturaUsuario = parseFloat(prompt("Qual é a sua altura?"));
 var imcUsuario = Math.round(pesoUsuario / (alturaUsuario * alturaUsuario));
 mostra("Olá " + nomeUsuario + ". O peso digitado é " + pesoUsuario + " e sua altura é " + alturaUsuario + ".");
 mostra("Seu IMC é " + imcUsuario + ".");
+pulaLinha();
+mostra("Agora vou praticar o While(Enquanto), que faz uma determinada estrutura se repetir.");
+mostra("E também farei a prática do método de repetição utilizando FOR para a construção de uma tabuada.");
+pulaLinha();
+//variáveis para anos de copa do mundo
+alert("Vamos contabilizar o número de Copa Do Mundo até o ano que você digitar")
+var anoCopa = 1930;
+var limite = parseInt(prompt("Diga um ano limite para mostrar os anos de Copa do Munda."))
+while(anoCopa <= limite){
+    mostra("Teve copa do mundo em " + anoCopa)
+    anoCopa = anoCopa + 4;
+
+}
+mostra("Fim");
+pulaLinha();
+//utilização do método de repetição FOR para a construção de uma tabuada. 
+//para o for existem 3 campos para preencher, 1 a variável, segundo a condição, e terceiro a variável mais 1
+for (var multiplicador = 1; multiplicador <= 10; multiplicador ++){
+    mostra(5 * multiplicador);
+};
+pulaLinha();
+pulaLinha();
+//utilizando a repetição while pra verificar média familiar de idade.
+alert("Vamos calcular a média de idade de sua família")
+var quantidadeFamilia = parseInt(prompt("Quantas pessoas tem sua familia? "));
+var numeroPessoas = 1;
+var somaIdade = 0;
+while(numeroPessoas <= quantidadeFamilia) {
+    
+    var idade = parseInt(prompt("Qual a idade do seu familiar?"));
+    somaIdade = somaIdade + idade;
+    numeroPessoas++;
+    
+}
+var mediaIdades = somaIdade / quantidadeFamilia;
+mostra("A média de idade da sua família é " + mediaIdades + ".");
+pulaLinha();
+pulaLinha();
